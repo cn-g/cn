ec0e44a772a4cc0828d79a8747c5703c8fb464c8
郭长平 <2046039989@qq.com>
2022-03-30 09:39:40 +0800

    feat:优化管理后台

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,7 +20,7 @@
                     </span>
                     <template #dropdown>
                         <el-dropdown-menu>
-                            <a href="https://github.com/lin-xin/vue-manage-system" target="_blank">
+                            <a href="https://github.com/cn-g/cn" target="_blank">
                                 <el-dropdown-item>项目仓库</el-dropdown-item>
                             </a>
                             <el-dropdown-item command="user">个人中心</el-dropdown-item>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -94,7 +94,7 @@ export default {
     position: relative;
     width: 100%;
     height: 100%;
-    background-image: url(../assets/img/login-bg.jpg);
+    background-image: url(../assets/img/blog-login.png);
     background-size: 100%;
 }
 .ms-title {
